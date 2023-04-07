68179b7540c38e74d0c8a08d04d1eb10ddc5e89d
Tanay Saxena <tanay.saxena002@gmail.com>
2023-04-07 11:44:30 +0530

    Primary Bean

--- Spring5Web DI/src/main/java/com/spring/depedencyInjection/controller/ConstructorInjectionController.java
+++ Spring5Web DI/src/main/java/com/spring/depedencyInjection/controller/ConstructorInjectionController.java
@@ -16,9 +16,16 @@ public class ConstructorInjectionController
 	
 	/*
 	 We don't have to tell spring explicitly to utilize the constructor , Hence its
-	 automatically  going to autowire. we don't have to provide the autowire annotation. 
+	 automatically  going to autowire. we don't have to provide the autowire and qualifier annotation. 
+	  
+	   In case of Primary annotation also , we doesn't need to provide autowire and qualifier 
+	   so we can comment below piece of codes.  @Qualifier("constructorGreetingServiceImpl")
+	   
+	   
 	   */
-	public ConstructorInjectionController(@Qualifier("constructorGreetingServiceImpl") GreetingService greetingService) {
+	
+
+	public ConstructorInjectionController(@Qualifier("constructorGreetingServiceImpl")  GreetingService greetingService) {
 		super();
 		this.greetingService = greetingService;
 	}
--- Spring5Web DI/src/main/java/com/spring/depedencyInjection/controller/MyController.java
+++ Spring5Web DI/src/main/java/com/spring/depedencyInjection/controller/MyController.java
@@ -2,11 +2,21 @@ package com.spring.depedencyInjection.controller;
 
 import org.springframework.stereotype.Controller;
 
+import com.spring.depedencyInjection.service.GreetingService;
+
 @Controller
 public class MyController {
+        
+	private final GreetingService greetingService;
+	
+	// For Constructor injected controller , there is no need for autowire and also no need for qualifier
+	public MyController(GreetingService greetingService) {
+		super();
+		this.greetingService = greetingService;
+	}
 
 	public String getGreetings()
 	{
-		return "Hello Worlds!!!";
+		return greetingService.sayGreetings();
 	}
 }
